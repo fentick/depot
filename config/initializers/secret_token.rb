81e14eca281d92d5a696ac04f4390ca38f768e7a
fentick <fentick_xv@msn.com>
2013-11-19 02:20:56 +0800

    Try again to push something

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '6909f70b0383bf30ac712545dda1f8044a3a9633142201ae6fb10941828e1bf258e02534b1cf46727a634b285808e1dfb2cbf1d59d610d9bbeeb59673112c799'
