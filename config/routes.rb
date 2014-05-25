217cc87bc7a9ec4bc28cb8ab03686c2efc877211
fentick <fentick_xv@msn.com>
2014-05-25 23:59:30 +0800

    Task I4: Adding a Sidebar, More Administration

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Depot::Application.routes.draw do
     delete 'logout' => :destroy
   end
 
-#  get "sessions/new"
+  get "sessions/new"
   get "sessions/create"
   get "sessions/destroy"
   resources :users
